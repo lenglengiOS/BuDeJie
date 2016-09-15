b9916d80daf80d1d77916e2493cf13d1ab92507c
lhl <lenghonglin91@163.com>
2016-09-15 12:13:00 +0800

    全屏滑动

--- 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Main(主要)/Controller/LHLNavigationViewController.m
+++ 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Main(主要)/Controller/LHLNavigationViewController.m
@@ -38,15 +38,34 @@
     [super viewDidLoad];
     
     
-    self.interactivePopGestureRecognizer.delegate = self;
+//    self.interactivePopGestureRecognizer.delegate = self;
+//    NSLog(@"%@", self.interactivePopGestureRecognizer);
+    
+    // initWithTarget:self.interactivePopGestureRecognizer.delegate 利用系统的滑动功能
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]
+                                   initWithTarget:self.interactivePopGestureRecognizer.delegate action:@selector(handleNavigationTransition:)];
+    [self.view addGestureRecognizer:pan];
+    
+    // 控制手势什么时候出发，只有是非根控制器的时候才触发
+    pan.delegate = self;
+    
+    // 禁止之前的手势
+    self.interactivePopGestureRecognizer.enabled = NO;
+    
     
 }
+
+/*
+ 
+ <UIScreenEdgePanGestureRecognizer: 0x7fe5f0f77790; state = Possible; delaysTouchesBegan = YES; view = <UILayoutContainerView 0x7fe5f0d28b90>; target= <(action=handleNavigationTransition:, target=<_UINavigationInteractiveTransition 0x7fe5f0f771e0>)>>
+ 
+ */
 
 
 #pragma mark - UIGestureRecognizerDelegate
 
+// 判断是否设置滑动返回，只有是非根控制器的时候才触发
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
-    
     
     // 非根控制器
     return self.childViewControllers.count > 1;
--- 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Setting/Controller/LHLSettingViewController.m
+++ 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Setting/Controller/LHLSettingViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     
     // 设置标题
     self.title = @"设置";
