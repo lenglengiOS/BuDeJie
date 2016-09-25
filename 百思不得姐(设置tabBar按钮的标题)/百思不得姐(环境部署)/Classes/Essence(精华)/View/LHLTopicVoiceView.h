73f9310458d1ec13694e70645e3e8d7f9893be2a
lhl <lenghonglin91@163.com>
2016-09-25 12:25:52 +0800

    中间内容的占位图片

---
+++ 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Essence(精华)/View/LHLTopicVoiceView.h
@@ -0,0 +1,17 @@
+//
+//  LHLTopicVideoView.h
+//  百思不得姐(环境部署)
+//
+//  Created by admin on 16/9/24.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LHLTopicsItem;
+@interface LHLTopicVoiceView : UIView
+
+/** 数据模型 */
+@property (nonatomic, strong) LHLTopicsItem *topic;
+
+@end
---
+++ 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Essence(精华)/View/LHLTopicVoiceView.m
@@ -0,0 +1,44 @@
+//
+//  LHLTopicVideoView.m
+//  百思不得姐(环境部署)
+//
+//  Created by admin on 16/9/24.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "LHLTopicVoiceView.h"
+#import <AFNetworking/AFNetworking.h>
+#import "UIImageView+WebCache.h"
+#import "LHLTopicsItem.h"
+
+@interface LHLTopicVoiceView ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *playcountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *voicetimeLabel;
+@end
+
+@implementation LHLTopicVoiceView
+
+- (void)awakeFromNib{
+    self.autoresizingMask = UIViewAutoresizingNone;
+}
+
+- (void)setTopic:(LHLTopicsItem *)topic{
+    _topic = topic;
+    // 设置图片
+    UIImage *placeholder = nil;
+    [self.imageView lhl_setOriginImage:topic.image1 thumbnailImage:topic.image0 placeholder:placeholder];
+    
+    // 播放数量
+    if (topic.playcount > 10000) { // 播放次数大于10000
+        self.playcountLabel.text = [NSString stringWithFormat:@"%.1lf万播放", topic.playcount /10000.0];
+    }else{ // 播放次数小于10000
+        self.playcountLabel.text = [NSString stringWithFormat:@"%zd播放", topic.playcount];
+    }
+    
+    self.voicetimeLabel.text = [NSString stringWithFormat:@"%02zd:%02zd", topic.voicetime / 60, topic.voicetime % 60];
+
+}
+
+
+@end
