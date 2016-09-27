9a0005a875c6ae09a5050c3970f12f72046a2ff0
lhl <lenghonglin91@163.com>
2016-09-27 09:52:05 +0800

    判断授权状态，如果是第一次访问相册，则在用户选择后才进行相册的创建和照片的存储

--- 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Essence(精华)/Controller/LHLSeeBigPictureViewController.m
+++ 百思不得姐(设置tabBar按钮的标题)/百思不得姐(环境部署)/Classes/Essence(精华)/Controller/LHLSeeBigPictureViewController.m
@@ -15,63 +15,12 @@
 @interface LHLSeeBigPictureViewController ()<UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *saveButton;
 @property (nonatomic, weak) UIImageView *imageView;
+- (PHAssetCollection *)createdCollection;
 @end
 
 @implementation LHLSeeBigPictureViewController
 
-/**
- *  保存图片到相机胶卷
- */
-- (IBAction)saveBtn:(id)sender {
-//    NSError *error = nil;
-////    1.保存图片到 相机胶卷
-//    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
-//        [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image];
-//    } error:&error];
-//    if (!error) { // 保存成功
-//        [SVProgressHUD showSuccessWithStatus:@"保存成功！"];
-//    }else{ // 保存失败
-//        [SVProgressHUD showErrorWithStatus:@"保存失败！"];
-//    }
-    
-    // 获得软件名字
-    NSString *title = [NSBundle mainBundle].infoDictionary[(NSString *)kCFBundleNameKey];
-    
-    // 抓取所有自定义相册
-    PHFetchResult<PHAssetCollection *> *collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-    
-    // 查找当前app对应的自定义相册
-    PHAssetCollection *createdCollection = nil;
-    for (PHAssetCollection *collection in collections) {
-        if ([collection.localizedTitle isEqualToString:title]) {
-            createdCollection = collection;
-            break;
-        }
-    }
-    
-    // 当前app的自定义相册没有被创建过
-    if (createdCollection == nil) {
-        NSError *error = nil;
-        NSString *title = [NSBundle mainBundle].infoDictionary[(NSString *)kCFBundleNameKey];
-        __block NSString *createdCollectionID = nil;
-        [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
-            // 2.创建自定义相册
-            createdCollectionID = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:title].placeholderForCreatedAssetCollection.localIdentifier;
-        } error:&error];
-        createdCollection = [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[createdCollectionID] options:nil].firstObject;
-    }
-    
-    LHLLog(@"%@", createdCollection);
-}
-
-/**
- *  退出看图
- */
-- (IBAction)backBtn:(id)sender {
-    
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
+#pragma mark - 初始化
 - (void)viewDidLoad {
     [super viewDidLoad];
 
@@ -106,6 +55,107 @@
     }
 }
 
+#pragma mark - 获得当前app的自定义相册
+- (PHAssetCollection *)createdCollection{
+    
+    // 获得软件名字
+    NSString *title = [NSBundle mainBundle].infoDictionary[(NSString *)kCFBundleNameKey];
+    
+    // 抓取所有自定义相册
+    PHFetchResult<PHAssetCollection *> *collections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    
+    // 查找当前app对应的自定义相册
+    for (PHAssetCollection *collection in collections) {
+        if ([collection.localizedTitle isEqualToString:title]) {
+            return collection;
+        }
+    }
+    
+    /** 当前app的自定义相册没有被创建过 **/
+    NSError *error = nil;
+    __block NSString *createdCollectionID = nil;
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        // 2.创建自定义相册
+        createdCollectionID = [PHAssetCollectionChangeRequest creationRequestForAssetCollectionWithTitle:title].placeholderForCreatedAssetCollection.localIdentifier;
+    } error:&error];
+    
+    if (error) return nil;
+    
+    // 根据唯一标示获得前面创建的相册
+    PHAssetCollection *collection = [PHAssetCollection fetchAssetCollectionsWithLocalIdentifiers:@[createdCollectionID] options:nil].firstObject;
+    return collection;
+    
+}
+
+#pragma mark - 监听点击
+/**
+ *  保存图片到相机胶卷
+ */
+- (IBAction)saveBtn:(id)sender {
+    
+    PHAuthorizationStatus oldStatus = [PHPhotoLibrary authorizationStatus];
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        if (status == PHAuthorizationStatusDenied) { // 用户拒绝当前app访问相册
+            if (oldStatus != PHAuthorizationStatusNotDetermined) {
+                LHLLog(@"提醒用户打开开关");
+            }
+        }else if (status == PHAuthorizationStatusAuthorized){ // 用户允许当前app访问相册
+            [self saveImageIntoAlbum];
+        }else if (status == PHAuthorizationStatusRestricted){ // 无法访问相册
+            [SVProgressHUD showErrorWithStatus:@"因系统原因，无法访问相册"];
+        }
+    }];
+    
+}
+- (void)saveImageIntoAlbum
+{
+    //    1.保存图片到 相机胶卷
+    NSError *error = nil;
+    
+    __block PHObjectPlaceholder *placeholder = nil;
+    
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        placeholder = [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image].placeholderForCreatedAsset;
+    } error:&error];
+    if (!error) { // 保存成功
+    }else{ // 保存失败
+        [SVProgressHUD showErrorWithStatus:@"保存失败！"];
+    }
+    
+    // 2.获得相册
+    PHAssetCollection *createdCollection = self.createdCollection;
+    if (createdCollection == nil) {
+        [SVProgressHUD showErrorWithStatus:@"创建相册失败！"];
+        return;
+    }
+    
+    // 3.天加刚才创建的图片到自定义相册
+    [[PHPhotoLibrary sharedPhotoLibrary] performChangesAndWait:^{
+        PHAssetCollectionChangeRequest *request = [PHAssetCollectionChangeRequest changeRequestForAssetCollection:createdCollection];
+        //        [request addAssets:@[placeholder]];
+        [request insertAssets:@[placeholder] atIndexes:[NSIndexSet indexSetWithIndex:0]];
+        
+        
+    } error:&error];
+    
+    // 最后的判断
+    if (error) {
+        [SVProgressHUD showErrorWithStatus:@"图片保存失败！"];
+    }else{
+        [SVProgressHUD showSuccessWithStatus:@"图片保存成功！"];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [SVProgressHUD dismiss];
+        });
+    }
+}
+
+/**
+ *  退出看图
+ */
+- (IBAction)backBtn:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 #pragma mark - <UIScrollViewDelegate>
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
     
